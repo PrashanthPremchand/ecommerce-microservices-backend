487900b845c1a65671b78884419977621c664b42
Prashanth <prashanthpremchand@gmail.com>
2026-09-25 16:16:54 +0530

    OpenAPI config

--- services/gateway/src/main/java/com/prashanth/gateway/config/OpenApiConfig.java
+++ services/gateway/src/main/java/com/prashanth/gateway/config/OpenApiConfig.java
@@ -1,4 +1,54 @@
 package com.prashanth.gateway.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Prashanth",
+                        email = "prashanth@mail.gmail",
+                        url = "http://exampleurl.com"
+                ),
+                description = "openApi documentation for book social network",
+                title = "openApi documentation",
+                version = "1.0",
+                license = @License(
+                        name = "License name",
+                        url = "http://somelincenseurl.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "http://sampleproduction.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
 public class OpenApiConfig {
 }
